44b3ec47313d2f8cf48d2f4a3177077ebb609949
Mark Simmons <mjsimmons@ucsd.edu>
2025-05-05 11:25:51 -0700

    sandboxing grammar

--- compile_lexd.sh
+++ compile_lexd.sh
@@ -2,10 +2,10 @@ lexicon=$1/lexicon.lexd
 att_gen=$1/generator.att
 hfst_gen=$1/generator.hfst
 analyzer=$1/analyzer.bin
-sed "s/\\High/'\u0301'/g" $lexicon > $lexicon.tmp
-# sed "/s\\Low/'\u0300'" -i $lexicon.tmp
-lexd $lexicon > $att_gen
-# rm $lexicon.tmp
+sed s/'\\High'/$'\u0301'/g $lexicon > $lexicon.tmp
+sed s/'\\Low'/$'\u0300'/g -i $lexicon.tmp
+lexd $lexicon.tmp > $att_gen
+rm $lexicon.tmp
 sed -e 's/[[:space:]]*$//g' -i $att_gen
 lt-comp rl $att_gen $analyzer
 hfst-txt2fst $att_gen -o $hfst_gen
